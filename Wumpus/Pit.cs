97da01a68738680a44a6cb1e36db3711716ed1d5
Mandurang <56937341+Mandurang@users.noreply.github.com>
2023-10-15 18:36:19 +0100

    Adde pit class

--- Wumpus/Pit.cs
+++ Wumpus/Pit.cs
@@ -6,8 +6,47 @@ using System.Threading.Tasks;
 
 namespace Wumpus
 {
-    internal class Pit
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace ConsoleApp1
     {
-        public int Room { get; set; }
+        public class Pit
+        {
+            public int PitX { get; set; }
+            public int PitY { get; set; }
+
+            public Pit(int pitX, int pitY)
+            {
+                PitX = pitX;
+                PitY = pitY;
+            }
+            public Pit() { }
+
+            public List<Pit> PlacePits(int quantityPit, Random random, char[][] MapSquare, int worldSize)
+            {
+                List<Pit> pits = new List<Pit>();
+
+                for (int i = 0; i < quantityPit; i++)
+                {
+                    int pitX, pitY;
+
+                    do
+                    {
+                        pitX = random.Next(worldSize);
+                        pitY = random.Next(worldSize);
+                    } while (MapSquare[pitX][pitY] != '_');
+                    MapSquare[pitX][pitY] = 'P';
+
+                    pits.Add(new Pit(pitX, pitY));
+
+                }
+                return pits;
+            }
+        }
     }
+
 }
