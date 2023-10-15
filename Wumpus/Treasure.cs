e99118010a7651313ed761befd21dfd86fbd299b
Mandurang <56937341+Mandurang@users.noreply.github.com>
2023-10-15 18:36:57 +0100

    added class treasure

---
+++ Wumpus/Treasure.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Treasure
+    {
+        public int TreasureX { get; set; }
+        public int TreasureY { get; set; }
+
+        public Treasure(int treasureX, int treasureY)
+        {
+            TreasureX = treasureX;
+            TreasureY = treasureY;
+        }
+
+        public Treasure() { }
+
+        public List<Treasure> PlaceTreasure(int quantityTreasure, Random random, char[][] MapSquare, int worldSize)
+        {
+            List<Treasure> treasures = new List<Treasure>();
+
+            for (int i = 0; i < quantityTreasure; i++)
+            {
+                int treasureX, treasureY;
+
+                do
+                {
+                    treasureX = random.Next(worldSize);
+                    treasureY = random.Next(worldSize);
+                } while (MapSquare[treasureX][treasureY] != '_');
+                MapSquare[treasureX][treasureY] = 'G';
+
+                treasures.Add(new Treasure(treasureX, treasureY));
+
+            }
+            return treasures;
+        }
+    }
+}
